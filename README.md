4246464c9cce72b73185538a5a87f76b6f500ef6
Cong <congusbongus@gmail.com>
2023-04-27 21:25:31 +1000

    Scrape as much info from page as possible

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Script to scrape, download and extract metadata from carts hosted on the TIC-80 
 ## Getting Started
 
 - Get [poetry](https://python-poetry.org/docs/#installation)
-- 
+- Run `scraper.py`
--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime as dt
 from pathlib import Path
 
 import aiofiles
@@ -23,12 +24,66 @@ async def main():
                     break
                 content = await resp.text()
             soup = BeautifulSoup(content, "html.parser")
+            # Scrape metadata from the cart page
+            heading = soup.find("h1")
+            category, title = heading.text.split(" > ")
+            attr_divs = heading.find_next_siblings("div")
+            desc = attr_divs[0].text
+            author = next(
+                div for div in attr_divs if div.text.startswith("made by ")
+            ).text.split("made by ")[1]
+            uploader_div = next(
+                div for div in attr_divs if div.text.startswith("uploaded by ")
+            )
+            uploader = uploader_div.text.split("uploaded by ")[1]
+            uploader_href = uploader_div.find("a").attrs["href"]
+            uploader_id = int(uploader_href.split("id=")[1])
+            uploader_link = TIC80_BASE_URL + uploader_href
+            added = dt.datetime.fromtimestamp(
+                int(
+                    next(div for div in attr_divs if div.text.startswith("added: "))
+                    .find("span", class_="date")
+                    .attrs["value"]
+                )
+                / 1000
+            )
+            updated = dt.datetime.fromtimestamp(
+                int(
+                    next(div for div in attr_divs if div.text.startswith("updated: "))
+                    .find("span", class_="date")
+                    .attrs["value"]
+                )
+                / 1000
+            )
+            rating = int(soup.find(id="rating-label").text)
+            text = heading.parent.find("p").text
+            dl_link = (
+                TIC80_BASE_URL
+                + soup.find("a", string="download cartridge").attrs["href"]
+            )
+            metadata.append(
+                {
+                    "cart_id": cart_id,
+                    "category": category,
+                    "title": title,
+                    "desc": desc,
+                    "author": author,
+                    "uploader": uploader,
+                    "uploader_id": uploader_id,
+                    "uploader_link": uploader_link,
+                    "url": url,
+                    "filename": dl_link.split("/")[-1],
+                    "added": added,
+                    "updated": updated,
+                    "rating": rating,
+                    "text": text,
+                }
+            )
 
             # Download carts
-            out_dir = (OUTPUT_DIR / "carts")
+            out_dir = OUTPUT_DIR / "carts"
             out_dir.mkdir(parents=True, exist_ok=True)
             path = out_dir / f"{cart_id}.tic"
-            dl_link = TIC80_BASE_URL + soup.find("a", string="download cartridge").attrs["href"]
             if path.exists():
                 print(f"Skipping {path=}")
             else:
@@ -39,11 +94,6 @@ async def main():
                             await f.write(await resp.read())
                         print(f"Downloaded {str(path)}")
 
-            metadata.append({
-                "cart_id": cart_id,
-                "url": url,
-                "filename": dl_link.split("/")[-1],
-            })
             cart_id += 1
     df = pd.DataFrame(metadata)
     df.to_csv(OUTPUT_DIR / "metadata.csv")
